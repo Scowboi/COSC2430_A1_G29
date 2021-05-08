d10bda04b68ad6941cdf6cbf16f26feb65ab110b
hyt3571 <s3741366@rmit.edu.vn>
2021-05-08 20:49:34 +0900

    update popup

--- COSC2430_Group29_A1_final_submit/Popup.js
+++ COSC2430_Group29_A1_final_submit/Popup.js
@@ -1,15 +1,45 @@
 
 
-var alert = window.open("", "myWindow", "width=400, height=200");
-function openWin() {
 
-// alert.document.write("<p>This is 'myWindow'</p>");
-alert.document.getElementByID("close").onclick =
-    function(){
-        alert.close();
+window.onload = function (){
+    popUp();
+
+    };
+
+
+
+    
+
+
+function setCookie(name,value){
+    document.cookie = infor(name) + "," +infor(value); +";"
+}
+
+function getCookie(name){
+    var cookie = document.cookie;
+    if (document.cookie != ""){
+        var cookieList = cookie.split(";");
+        for (var index in cookieList){
+            var cookieName = cookieList[index].split(",");
+            if(cookieName[0] == "popupChecked"){
+                return cookieName[1];
+            }
+        }
     }
 }
 
+function popUp(){
+    var cookieSearch = getCookie("popupChecked");
+    if (cookieSearch != "Y"){
+        window.open("mallPopup.html", "myWindow", "width=400, height=200");
+    }
+    
+}
 
 
- 
+function ClosePopup() { 
+
+        setCookie("popupChecked", "Y");
+        window.close();
+    
+}
